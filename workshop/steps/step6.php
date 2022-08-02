c490c9666d8c429403f9fa2e8e027b2f2d40a51e
Simon Ng <simongcc@gmail.com>
2022-08-02 16:21:55 +0800

    update step6

--- workshop/steps/step6.php
+++ workshop/steps/step6.php
@@ -10,36 +10,28 @@ add_action(
 			global $product;
 			$html         = '';
 
-			if( $product->get_stock_quantity() < 5 ) {
-				?>
-				<div class="" style="background: red; color: white; display: inline-block; padding: 0.45em 0.85em; float: right;">
-				<?php
+			?>
+			<div class="" style="background: red; color: white; display: inline-block; padding: 0.45em 0.85em; float: right;">
+			<?php
+
+			if( $product->get_stock_quantity() < 5 && $product->get_stock_quantity() > 0 ) {
 				echo '尚餘少量';
-				?>
-				</div>
-				<?php
 			}
 
-			// $availability = $product->get_availability();
-
-			// var_dump($product->get_stock_quantity());
-
-			// if ( ! empty( $availability['availability'] ) ) {
-			// 	ob_start();
-
-			// 	wc_get_template(
-			// 		'single-product/stock.php',
-			// 		array(
-			// 			'product'      => $product,
-			// 			'class'        => $availability['class'],
-			// 			'availability' => $availability['availability'],
-			// 		)
-			// 	);
-
-			// 	$html = ob_get_clean();
-			// }
-
-		echo $html;
+			if( $product->get_stock_quantity() === 0 ) {
+				$availability = $product->get_availability();
+	
+				if ( ! empty( $availability['availability'] ) ) {
+					$class = $availability['class'];
+					$availability = $availability['availability'];
+					?>
+					<p class="stock <?php echo esc_attr( $class ); ?>" style="color: inherit;     margin: 0;"><?php echo wp_kses_post( $availability ); ?></p>
+					<?php
+				}
+			}
+		?>
+		</div>
+		<?php
 	}, 30);
 
 remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
