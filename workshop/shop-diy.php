d5ec661c88ff25af2b166256a826aab5cd5f0410
Simon Ng <simongcc@gmail.com>
2022-08-02 10:06:49 +0800

    add basic plugin template

---
+++ workshop/shop-diy.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Workshop_Shop_DIY
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Learnmore Workshop - Shop DIY
+Plugin URI: https://learnmore.com.hk/
+Description: This is a WordPress plugin demonstration in the Learnmore.com.hk Workshop for adding custom features/layout to WooCommerce.
+Author: Simon Ng
+Version: 1.0.0
+Author URI: http://zenrity.com/
+*/
+
+/** Add any code below. */
