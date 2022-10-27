bf34886809fc0947f75c80cf963269e11a4a2999
Simon Ng <simongcc@gmail.com>
2022-10-27 12:29:35 +0800

    update version

--- workshop/shop-diy.php
+++ workshop/shop-diy.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * @package Workshop_Shop_DIY
- * @version 1.0.0
- */
 /*
 Plugin Name: Learnmore Workshop - Shop DIY
 Plugin URI: https://learnmore.com.hk/
 Description: This is a WordPress plugin demonstration in the Learnmore.com.hk Workshop for adding custom features/layout to WooCommerce.
 Author: Simon Ng
-Version: 1.0.0
+Version: 1.0.1
 Author URI: http://zenrity.com/
 */
 
